71dc0fb322c05172f704c618872d066c81c6ab7a
Sofia1226 <75131893+Sofia1226@users.noreply.github.com>
2020-12-31 18:22:06 +0530

    Add files via upload

---
+++ package.js
@@ -0,0 +1,10 @@
+//class Package {
+    //constructor(x,y,width,height){
+        //var option = {
+            //'restitution' :0.8,
+           // 'friction' : 0.3,
+            //'density' : 1.0
+        //}
+
+   // }
+//}
